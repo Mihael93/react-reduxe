7c2b16f64ca2efb694014c0c20834c97b48a0f6f
Mihael93 <vynnykm93@gmail.com>
2022-07-22 21:58:20 +0300

    55 lessons up

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,46 +1,74 @@
 // import React, { Component } from 'react';
 import axios from "axios";
-import s from "./Users.module.css";
+import styles from "./Users.module.css";
 import userPhoto from '../../assets/images/user.png';
 import React from "react";
 
 class Users extends React.Component {
-   getUsers = () => {
-      if (this.props.users.length === 0) {
-         axios.get('https://social-network.samuraijs.com/api/1.0/users')
-            .then(response => {
-               this.props.setUsers(response.data.items);
-            });
-      }
+
+   componentDidMount() {
+      axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
+         .then(response => {
+            this.props.setUsers(response.data.items);
+            this.props.setTotalUsersCount(response.data.totalCount);
+         });
+   }
+
+   onPageChanged = (pageNumber) => {
+      this.props.setCurrentPage(pageNumber);
+      axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`)
+         .then(response => {
+            this.props.setUsers(response.data.items);
+         });
    }
 
    render() {
+
+      let pagesCount = Math.ceil(this.props.totalUsersCount / this.props.pageSize);
+      let pages = [];
+      for (let i = 1; i <= pagesCount; i++) {
+         pages.push(i);
+         if (i === 20) break;
+      }
+
+      // let curP = this.props.currentPage;
+      // let curPF = ((curP - 5) < 0) ? 0 : curP - 5;
+      // let curPL = curP + 5;
+      // let slicedPages = pages.slice(curPF, curPL);
+
       return (
          <div>
-            <button onClick={this.getUsers}>Get Users</button>
+            <div>
+               {pages.map(page => {
+                  return <span className={this.props.currentPage === page && styles.selectedPage}
+                     onClick={(event) => { this.onPageChanged(page) }}>{page}</span>
+               })}
+            </div>
             {
-               this.props.users.map(user => <div key={user.id}>
-                  <span>
-                     <div>
-                        <img src={user.photos.small != null ? user.photos.small : userPhoto} alt="user" className={s.userPhoto} />
-                     </div>
-                     <div>
-                        {user.follow
-                           ? <button onClick={() => { this.props.unfollow(user.id) }}>Unfollow</button>
-                           : <button onClick={() => { this.props.follow(user.id) }}>Follow</button>}
-                     </div>
-                  </span>
-                  <span>
+               this.props.users.map(user =>
+                  <div key={user.id}>
                      <span>
-                        <div>{user.name}</div>
-                        <div>{user.status}</div>
+                        <div>
+                           <img src={user.photos.small != null ? user.photos.small : userPhoto}
+                              alt="user" className={styles.userPhoto} />
+                        </div>
+                        <div>
+                           {user.follow
+                              ? <button onClick={() => { this.props.unfollow(user.id) }}>Unfollow</button>
+                              : <button onClick={() => { this.props.follow(user.id) }}>Follow</button>}
+                        </div>
                      </span>
                      <span>
-                        <div>{"user.location.country"}</div>
-                        <div>{"user.location.city"}</div>
+                        <span>
+                           <div>{user.name}</div>
+                           <div>{user.status}</div>
+                        </span>
+                        <span>
+                           <div>{"user.location.country"}</div>
+                           <div>{"user.location.city"}</div>
+                        </span>
                      </span>
-                  </span>
-               </div>)
+                  </div>)
             }
          </div>
       )
--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -1,27 +1,25 @@
 const FOLLOW = 'FOLLOW';
 const UNFOLLOW = 'UNFOLLOW';
 const SET_USERS = 'SET_USERS';
+const SET_CURRENT_PAGE = 'SET_CURRENT_PAGE';
+const SET_TOTAL_USERS_COUNT = 'SET_TOTAL_USERS_COUNT';
 
 let initialState = {
-   users: [
-      // {
-      //    id: 1, photoUrl: "https://www.pngarts.com/files/11/Avatar-PNG-Free-Download.png",
-      //    follow: true, fullName: 'Mihael', status: 'I am boss', location: { country: 'Ukraine', city: 'Zaporizhia' }
-      // },
-      // {
-      //    id: 2, photoUrl: "https://www.pngarts.com/files/11/Avatar-PNG-Free-Download.png",
-      //    follow: false, fullName: 'Quentin', status: 'I am boss to', location: { country: 'Ukraine', city: 'Kyiv' }
-      // },
-      // {
-      //    id: 3, photoUrl: "https://www.pngarts.com/files/11/Avatar-PNG-Free-Download.png",
-      //    follow: false, fullName: 'Shara', status: 'I am boss tho', location: { country: 'USA', city: 'Philadelphia' }
-      // },
-      // {
-      //    id: 4, photoUrl: "https://www.pngarts.com/files/11/Avatar-PNG-Free-Download.png",
-      //    follow: true, fullName: 'Malenia', status: 'I am boss ayaya', location: { country: 'USA', city: 'Texas' }
-      // },
-   ],
+   users: [],
+   pageSize: 5,
+   totalUsersCount: 0,
+   currentPage: 1,
 };
+
+// case SET_USERS: {
+//    if (state.users.length > 0) {
+//       return state
+//    }
+//    return {
+//       ...state,
+//       users: [...state.users, ...action.users],
+//    }
+// }
 
 const usersReducer = (state = initialState, action) => {
    switch (action.type) {
@@ -46,15 +44,18 @@ const usersReducer = (state = initialState, action) => {
             })
          }
       case SET_USERS: {
-         if (state.users.length > 0) {
-            return state
-         }
+         return { ...state, users: action.users }
+      }
+      case SET_CURRENT_PAGE: {
          return {
-            ...state,
-            users: [...state.users, ...action.users],
+            ...state, currentPage: action.currentPage
          }
       }
-      // return { ...state, users: [...state.users, ...action.users] }
+      case SET_TOTAL_USERS_COUNT: {
+         return {
+            ...state, totalUsersCount: action.count
+         }
+      }
       default:
          return state;
    }
@@ -63,5 +64,7 @@ const usersReducer = (state = initialState, action) => {
 export const followActionCreator = (userId) => ({ type: FOLLOW, userId });
 export const unfollowActionCreator = (userId) => ({ type: UNFOLLOW, userId });
 export const setUsersActionCreator = (users) => ({ type: SET_USERS, users });
+export const setCurrentPageAC = (currentPage) => ({ type: SET_CURRENT_PAGE, currentPage: currentPage });
+export const setTotalUsersCountAC = (totalUsersCount) => ({ type: SET_TOTAL_USERS_COUNT, count: totalUsersCount });
 
 export default usersReducer;
